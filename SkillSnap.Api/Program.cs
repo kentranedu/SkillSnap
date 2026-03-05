ae096a7078c21fb944ab157ff80aa1dbccc7edcf
kentranedu <ken.tran.edu@outlook.de>
2026-03-05 10:51:11 +0100

    Act2_Step2_Enable CORS in the API

--- SkillSnap.Api/Program.cs
+++ SkillSnap.Api/Program.cs
@@ -12,6 +12,17 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<SkillSnapContext>(options =>
     options.UseSqlite(connectionString));
 
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowClient", policy =>
+        {
+            policy.WithOrigins("https://localhost:5001")
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowClient");
 
 app.MapControllers();
 
